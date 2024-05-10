57457f3a01a9ec0d211af5449f2956b9af1a67cb
ranagithubrr <mr6430119@gmail.com>
2024-05-10 20:58:42 +0600

    Feat: navbar complete

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import { IoNotificationsOutline } from "react-icons/io5";
+import { FaBars, FaTimes, FaUserCircle } from "react-icons/fa";
+import './Navbar.css';
+import { useState } from "react";
+
+const Navbar = () => {
+    const [showMobileMenu, setshowMobileMenu] = useState(true)
+    return (
+        <div className="navbar">
+            <div className="navbarInner">
+                <div className="navBarLeftSide">
+                    <span>Dashboard</span>
+                    <ul className={showMobileMenu && 'showMobileMenu'}>
+                        <span className="timesIcon" onClick={() => setshowMobileMenu(false)}><FaTimes /></span>
+                        <li><a href="#">Master Price</a></li>
+                        <li><a href="#">Custom Price</a></li>
+                        <li><a href="#">Calender</a></li>
+                        <li><a href="#">Reports</a></li>
+                    </ul>
+                </div>
+                <div className="navBarRightSide">
+                    <span>
+                        <IoNotificationsOutline />
+                    </span>
+                    <span>
+                        <FaUserCircle />
+                    </span>
+                    <span className="barIcon" onClick={() => setshowMobileMenu(true)}>
+                        <FaBars />
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
